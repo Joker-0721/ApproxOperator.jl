cfc720388882d268aa2fbedd2e2dd83554e4c132
Joker-0721 <joe010104@gmail.com>
2025-07-02 16:22:39 +0800

    2025/07/02

--- src/operation/stokes.jl
+++ src/operation/stokes.jl
@@ -16,8 +16,8 @@ function ∫∫μ∇u∇vdxdy(aᵤ::T,k::AbstractMatrix{Float64}) where T<:Abstr
                 J = xⱼ.𝐼
                 # 粘性项贡献：μ ∫ (∇u_x ⋅ ∇v_x + ∇u_y ⋅ ∇v_y) dΩ
                 k[2I-1,2J-1] += μ * (2*B₁[i]*B₁[j] + B₂[i]*B₂[j]) * 𝑤
-                k[2I,2J-1]   += μ * (B₁[i]*B₂[j]) * 𝑤
-                k[2I-1,2J]   += μ * (B₂[i]*B₁[j]) * 𝑤
+                k[2I-1,2J]   += μ * (B₁[i]*B₂[j] + B₂[i]*B₁[j]) * 𝑤 / 2
+                k[2I,2J-1]   += μ * (B₂[i]*B₁[j] + B₁[i]*B₂[j]) * 𝑤 / 2
                 k[2I,2J]     += μ * (B₁[i]*B₁[j] + 2*B₂[i]*B₂[j]) * 𝑤
             end
         end
